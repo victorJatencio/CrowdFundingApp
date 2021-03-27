b4b4fa0453fb9bb498cad2ae47f1bddb340f6029
victor atencio <victorj.atencio@gmail.com>
2021-03-27 18:07:00 -0400

    modal done

--- js/script.js
+++ js/script.js
@@ -14,3 +14,20 @@ const handleCollapseMenu = () => {
 
 burgerMenu.addEventListener('click', handleMobileMenu);
 closeMenu.addEventListener('click', handleCollapseMenu);
+
+// Pledge Options
+const backProjectBtn = document.getElementById('back__proj');
+const collapseModal = document.querySelector('.close__modal');
+const modal = document.querySelector('.overlay');
+
+const handleOpenModal = () => {
+  modal.classList.remove('active__modal');
+  modal.classList.add('active__modal');
+}
+
+const handleModalCollapse = () => {
+  modal.classList.remove('active__modal');
+}
+
+backProjectBtn.addEventListener('click', handleOpenModal);
+collapseModal.addEventListener('click', handleModalCollapse);
